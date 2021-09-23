75fa8f93f440f86d636b22602d924d1db762f468
Omar Eladawy <omar.eladawy@tum.de>
2021-09-23 14:38:04 +0200

    added an under construction html page

--- api.py
+++ api.py
@@ -1,20 +1,19 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template
 import os
 import firebase_admin
 from firebase_admin import credentials
 
+
+app = Flask(__name__, template_folder='')
+
+
 cred = credentials.Certificate("Key.json")
 firebase_admin.initialize_app(cred)
-
-app = Flask(__name__,
-            static_url_path='',
-            static_folder='web/static',
-            template_folder='web/templates')
 
 
 @app.route('/')
 def hello():
-    return render_template('index.html')
+    return render_template('home.html')
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
