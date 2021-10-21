fed286551ef0eccfaf1ec5a1e1083ff7a46691af
Omar Eladawy <omar.eladawy@tum.de>
2021-10-21 14:51:14 +0200

    added hacky authentication

--- run_streamlit_demo.py
+++ run_streamlit_demo.py
@@ -22,71 +22,110 @@ dt = DecisionTree()
 image = Image.open('Pre_ex.png')
 st.title('Mediwell Demo board')
 
-with st.form(key="Form"):
-    exercise_level = st.radio(
-         "What is your current exercise level or hypoglycemia risk",
-        (ex_enum.EX_1, ex_enum.EX_2, ex_enum.EX_3))
 
-    if exercise_level == ex_enum.EX_1:
-        ex_level = 'ex_1'
-        print('ex_level = ', ex_level)
-    elif exercise_level == ex_enum.EX_2:
-        ex_level = 'ex_2'
-        print('ex_level = ', ex_level)
-    elif exercise_level == ex_enum.EX_3:
-        ex_level = 'ex_3'
-        print('ex_level = ', ex_level)
 
-    else:
-        raise AssertionError
-    # components.iframe("https://giphy.com/gifs/reaction-wwe-wrestling-UiBmJv6Hh6FfW/fullscreen")
+def is_authenticated(password):
+    return password == "mediwell-2021"
 
-    current_glucose = st.number_input('What is your current glucose value in mg/dl')
 
-    current_trend = st.radio(
-        'What is your current glucose trend',
-        (trends_enum.UP,
-         trends_enum.GOING_UP,
-         trends_enum.STABLE,
-         trends_enum.GOING_DOWN,
-         trends_enum.DOWN)
-    )
+def generate_login_block():
+    block1 = st.empty()
+    block2 = st.empty()
 
-    submitted = st.form_submit_button()
-    dt.set_exercise_level(ex_level)
+    return block1, block2
 
-if submitted:
-    res = dt.eval_glucose(current_glucose, current_trend)
-    with st.spinner("🤓 Getting Recommendation"):
-        time.sleep(1)
 
-    col1, col2 = st.columns(2)
-    col1.subheader('Expecting increase in glucose?')
-    col2.subheader('Expecting decrease in glucose?')
+def clean_blocks(blocks):
+    for block in blocks:
+        block.empty()
 
-    if res['color'] == 'green':
-        col1.success(
-            f"➡️ {res['case_increase']}"
-        )
-        col2.success(
-            f" ➡️ {res['case_decrease']}"
-        )
-    elif res['color'] == 'yellow' or res['color'] == 'dark_yellow':
-        col1.warning(
-            f" ➡️ {res['case_increase']}"
-        )
-        col2.warning(
-            f" ➡️ {res['case_decrease']}"
-        )
-    elif res['color'] == 'red':
-        col1.error(
-            f" ➡️ {res['case_increase']}"
-        )
-        col2.error(
-            f" ➡️ {res['case_decrease']}"
+
+def login(blocks):
+    blocks[0].markdown("""
+            <style>
+                input {
+                    -webkit-text-security: disc;
+                }
+            </style>
+        """, unsafe_allow_html=True)
+    return blocks[1].text_input('Password')
+
+def main():
+    with st.form(key="Form"):
+        exercise_level = st.radio(
+             "What is your current exercise level or hypoglycemia risk",
+            (ex_enum.EX_1, ex_enum.EX_2, ex_enum.EX_3))
+
+        if exercise_level == ex_enum.EX_1:
+            ex_level = 'ex_1'
+            print('ex_level = ', ex_level)
+        elif exercise_level == ex_enum.EX_2:
+            ex_level = 'ex_2'
+            print('ex_level = ', ex_level)
+        elif exercise_level == ex_enum.EX_3:
+            ex_level = 'ex_3'
+            print('ex_level = ', ex_level)
+
+        else:
+            raise AssertionError
+        # components.iframe("https://giphy.com/gifs/reaction-wwe-wrestling-UiBmJv6Hh6FfW/fullscreen")
+
+        current_glucose = st.number_input('What is your current glucose value in mg/dl')
+
+        current_trend = st.radio(
+            'What is your current glucose trend',
+            (trends_enum.UP,
+             trends_enum.GOING_UP,
+             trends_enum.STABLE,
+             trends_enum.GOING_DOWN,
+             trends_enum.DOWN)
         )
 
+        submitted = st.form_submit_button()
+        dt.set_exercise_level(ex_level)
 
-# components.html("""<hr style="height:5px;border:none;color:#333;background-color:#333;" /> """)
-with st.sidebar.expander("See Reference Chart"):
-    st.image(image)
+    if submitted:
+        res = dt.eval_glucose(current_glucose, current_trend)
+        with st.spinner("🤓 Getting Recommendation"):
+            time.sleep(1)
+
+        col1, col2 = st.columns(2)
+        col1.subheader('Expecting increase in glucose?')
+        col2.subheader('Expecting decrease in glucose?')
+
+        if res['color'] == 'green':
+            col1.success(
+                f"➡️ {res['case_increase']}"
+            )
+            col2.success(
+                f" ➡️ {res['case_decrease']}"
+            )
+        elif res['color'] == 'yellow' or res['color'] == 'dark_yellow':
+            col1.warning(
+                f" ➡️ {res['case_increase']}"
+            )
+            col2.warning(
+                f" ➡️ {res['case_decrease']}"
+            )
+        elif res['color'] == 'red':
+            col1.error(
+                f" ➡️ {res['case_increase']}"
+            )
+            col2.error(
+                f" ➡️ {res['case_decrease']}"
+            )
+
+
+    # components.html("""<hr style="height:5px;border:none;color:#333;background-color:#333;" /> """)
+    with st.sidebar.expander("See Reference Chart"):
+        st.image(image)
+
+
+login_blocks = generate_login_block()
+password = login(login_blocks)
+
+if is_authenticated(password):
+    clean_blocks(login_blocks)
+    main()
+elif password:
+    st.info("Please enter a valid password")
