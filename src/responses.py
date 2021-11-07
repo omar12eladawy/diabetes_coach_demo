d783dd9363e62b13b705d53f67825f7270f246d4
Omar Eladawy <omar.eladawy@tum.de>
2021-11-07 15:01:36 +0100

    api ready

--- src/responses.py
+++ src/responses.py
@@ -38,8 +38,8 @@ class ResponseFactory():
                 'case_decrease': {
                     'text_AE': 'Not ready for Exercise.',
                     'text_other': 'Not ready for Exercise.',
-                    'insulin_rec': 'Insulin Correction needed!',
-                    'insulin_rec': 'Insulin Correction needed!',
+                    'insulin_rec_AE': 'Insulin Correction needed!',
+                    'insulin_rec_other': 'Insulin Correction needed!',
                     'food_rec': None,
                 },
                 'color': 'red'
@@ -49,10 +49,10 @@ class ResponseFactory():
             self.response_dict[self.response_enum.RESPONSE_1] = {
                 # 'case_increase': 'Can start AE ',
                 'case_decrease': {
-                    'text_AE': 'Can start exercise immediately ',
-                    'text_other': 'Follow the insulin recommendation before starting!',
-                    'insulin_rec': 'Consider Insulin Correction with 50% of the regular factor.',
-                    'insulin_rec': 'Consider Insulin Correction with 50% of the regular factor.',
+                    'text_AE': 'Can start exercise! ',
+                    'text_other': 'Can start exercise!',
+                    'insulin_rec_AE': 'Consider Insulin Correction with 50% of the regular factor.',
+                    'insulin_rec_other': 'Consider Insulin Correction with 50% of the regular factor.',
                     'food_rec': None,
                 },
                 'color': 'dark_yellow'
@@ -60,11 +60,11 @@ class ResponseFactory():
             self.response_dict[self.response_enum.RESPONSE_2] = {
                 # 'case_increase': 'Can start AE ',
                 'case_decrease': {
-                    'text_AE': 'Can start exercise immediately ',
-                    'text_other': 'Follow the insulin recommendation before starting!',
-                    'insulin_rec': 'Consider Insulin Correction with 50% of the regular factor.',
-                    'insulin_rec': 'Consider Insulin Correction with 50% of the regular factor.',
-                    'food_rec': None
+                    'text_AE': 'Can start exercise! ',
+                    'text_other': 'Can start exercise!',
+                    'insulin_rec_AE': 'Consider Insulin Correction with 50% of the regular factor.',
+                    'insulin_rec_other': 'Consider Insulin Correction with 50% of the regular factor.',
+                    'food_rec': None,
                 },
                 'color': 'dark_yellow'
             }
@@ -73,8 +73,8 @@ class ResponseFactory():
                 'case_decrease': {
                     'text_AE': 'Can start all Ex',
                     'text_other': 'Can start all Ex',
-                    'insulin_rec': None,
-                    'insulin_rec': None,
+                    'insulin_rec_AE': None,
+                    'insulin_rec_other': None,
                     'food_rec': None
                 },
                 'color': 'dark_yellow'
@@ -89,9 +89,9 @@ class ResponseFactory():
                 # 'color': 'yellow'
                 'case_decrease': {
                     'text_AE': 'Can start exercise!',
-                    'text_other': 'Can start exercise!',
-                    'insulin_rec': None,
-                    'insulin_rec': None,
+                    'text_other': 'Can start exercise, but consider insulin correction',
+                    'insulin_rec_AE': None,
+                    'insulin_rec_other': 'Consider Insulin Correction with 50% of the regular factor.',
                     'food_rec': None
                 },
                 'color': 'yellow'
@@ -99,107 +99,215 @@ class ResponseFactory():
 
             #todo: refactor the dict.
             self.response_dict[self.response_enum.RESPONSE_5] = {
-                'case_increase': 'Can start all EX. Consider insulin correction for RT, HIIT',
-                'case_decrease': 'Can start all EX',
-                'insulin_rec': None,
-                'food_rec': None,
+                'case_decrease': {
+                    'text_AE': 'Can start exercise!',
+                    'text_other': 'Can start exercise!',
+                    'insulin_rec_AE': None,
+                    'insulin_rec_other': None,
+                    'food_rec': None
+                },
                 'color': 'yellow'
             }
             self.response_dict[self.response_enum.RESPONSE_6] = {
-                'case_increase': 'Can start all Ex',
-                'case_decrease': 'Can start all Ex',
-                'insulin_rec': None,
-                'food_rec': None,
+                # 'case_increase': 'Can start all Ex',
+                # 'case_decrease': 'Can start all Ex',
+                # 'insulin_rec': None,
+                # 'food_rec': None,
+                # 'color': 'yellow'
+                'case_decrease': {
+                    'text_AE': 'Can start exercise!',
+                    'text_other': 'Can start exercise!',
+                    'insulin_rec_AE': None,
+                    'insulin_rec_other': None,
+                    'food_rec': None
+                },
                 'color': 'yellow'
             }
 
             #Green area
             self.response_dict[self.response_enum.RESPONSE_7] = {
-                'case_increase': 'Can start all Ex',
-                'case_decrease': 'Can start all Ex',
-                'insulin_rec': None,
-                'food_rec': None,
+                # 'case_increase': 'Can start all Ex',
+                # 'case_decrease': 'Can start all Ex',
+                # 'insulin_rec': None,
+                # 'food_rec': None,
+                # 'color': 'green'
+                'case_decrease': {
+                    'text_AE': 'Can start exercise!',
+                    'text_other': 'Can start exercise!',
+                    'insulin_rec_AE': None,
+                    'insulin_rec_other': None,
+                    'food_rec': None
+                },
                 'color': 'green'
             }
             self.response_dict[self.response_enum.RESPONSE_8] = {
-                'case_increase': 'Can start all Ex',
-                'case_decrease': 'Can start all Ex. 15g CHO',
-                'insulin_rec': None,
-                'food_rec': None,
+                # 'case_increase': 'Can start all Ex',
+                # 'case_decrease': 'Can start all Ex. 15g CHO',
+                # 'insulin_rec': None,
+                # 'food_rec': None,
+                # 'color': 'green'
+                'case_decrease': {
+                    'text_AE': 'Can start exercise!',
+                    'text_other': 'Can start exercise!',
+                    'insulin_rec_AE': None,
+                    'insulin_rec_other': None,
+                    'food_rec': '15'
+                },
                 'color': 'green'
             }
 
             #Yellow Area
             self.response_dict[self.response_enum.RESPONSE_9] = {
-                'case_increase': 'Can start all Ex',
-                'case_decrease': 'Can start all Ex. 15 g CHO',
-                'insulin_rec': None,
-                'food_rec': None,
+                # 'case_increase': 'Can start all Ex',
+                # 'case_decrease': 'Can start all Ex. 15 g CHO',
+                # 'insulin_rec': None,
+                # 'food_rec': None,
+                # 'color': 'yellow'
+                'case_decrease': {
+                    'text_AE': 'Can start exercise!',
+                    'text_other': 'Can start exercise!',
+                    'insulin_rec_AE': None,
+                    'insulin_rec_other': None,
+                    'food_rec': '15'
+                },
                 'color': 'yellow'
             }
             self.response_dict[self.response_enum.RESPONSE_10] = {
-                'case_increase': 'Can start all Ex. 10 g CHO',
-                'case_decrease': 'Can start all Ex. 20 g CHO',
-                'insulin_rec': None,
-                'food_rec': None,
+                # 'case_increase': 'Can start all Ex. 10 g CHO',
+                # 'case_decrease': 'Can start all Ex. 20 g CHO',
+                # 'insulin_rec': None,
+                # 'food_rec': None,
+                # 'color': 'yellow'
+                'case_decrease': {
+                    'text_AE': 'Can start exercise!',
+                    'text_other': 'Can start exercise!',
+                    'insulin_rec_AE': None,
+                    'insulin_rec_other': None,
+                    'food_rec': '20'
+                },
                 'color': 'yellow'
             }
             self.response_dict[self.response_enum.RESPONSE_11] = {
-                'case_increase': 'Delay all Ex. 15 g CHO',
-                'case_decrease': 'Delay all Ex. 25 g CHO',
-                'insulin_rec': None,
-                'food_rec': None,
+                # 'case_increase': 'Delay all Ex. 15 g CHO',
+                # 'case_decrease': 'Delay all Ex. 25 g CHO',
+                # 'insulin_rec': None,
+                # 'food_rec': None,
+                # 'color': 'yellow'
+                'case_decrease': {
+                    'text_AE': 'Delay exercise at least until reaching 90mg/dl and ➡️, ↗️ or ⬆️!',
+                    'text_other': 'Delay exercise at least until reaching 90mg/dl and ➡️, ↗️ or ⬆️!',
+                    'insulin_rec_AE': None,
+                    'insulin_rec_other': None,
+                    'food_rec': '25'
+                },
                 'color': 'yellow'
             }
             self.response_dict[self.response_enum.RESPONSE_12] = {
-                'case_increase': 'Delay all Ex. 20 g CHO',
-                'case_decrease': 'Delay all Ex. 30 g CHO',
-                'insulin_rec': None,
-                'food_rec': None,
+                # 'case_increase': 'Delay all Ex. 20 g CHO',
+                # 'case_decrease': 'Delay all Ex. 30 g CHO',
+                # 'insulin_rec': None,
+                # 'food_rec': None,
+                # 'color': 'yellow'
+                'case_decrease': {
+                    'text_AE': 'Delay exercise at least until reaching 90mg/dl and ➡️, ↗️ or ⬆️!',
+                    'text_other': 'Delay exercise at least until reaching 90mg/dl and ➡️, ↗️ or ⬆️!',
+                    'insulin_rec_AE': None,
+                    'insulin_rec_other': None,
+                    'food_rec': '30'
+                },
                 'color': 'yellow'
             }
 
             #Dark Yellow Area
             self.response_dict[self.response_enum.RESPONSE_13] = {
-                'case_increase': 'Can start all Ex. 10 g CHO',
-                'case_decrease': 'Delay all Ex. 20 g CHO',
-                'insulin_rec': None,
-                'food_rec': None,
+                # 'case_increase': 'Can start all Ex. 10 g CHO',
+                # 'case_decrease': 'Delay all Ex. 20 g CHO',
+                # 'insulin_rec': None,
+                # 'food_rec': None,
+                # 'color': 'dark_yellow'
+                'case_decrease': {
+                    'text_AE': 'Delay exercise at least until reaching 90mg/dl and ➡️, ↗️ or ⬆️!',
+                    'text_other': 'Delay exercise at least until reaching 90mg/dl and ➡️, ↗️ or ⬆️!',
+                    'insulin_rec_AE': None,
+                    'insulin_rec_other': 'Insulin Correction needed!',
+                    'food_rec': '20'
+                },
                 'color': 'dark_yellow'
             }
             self.response_dict[self.response_enum.RESPONSE_14] = {
-                'case_increase': 'Delay all Ex. 15 g CHO',
-                'case_decrease': 'Delay all Ex. 25 g CHO',
+                # 'case_increase': 'Delay all Ex. 15 g CHO',
+                # 'case_decrease': 'Delay all Ex. 25 g CHO',
+                # 'color': 'dark_yellow'
+                'case_decrease': {
+                    'text_AE': 'Delay exercise at least until reaching 90mg/dl and ➡️, ↗️ or ⬆️!',
+                    'text_other': 'Delay exercise at least until reaching 90mg/dl and ➡️, ↗️ or ⬆️!',
+                    'insulin_rec_AE': None,
+                    'insulin_rec_other': 'Insulin Correction needed!',
+                    'food_rec': '25'
+                },
                 'color': 'dark_yellow'
             }
             self.response_dict[self.response_enum.RESPONSE_15] = {
-                'case_increase': 'Delay all Ex. 20 g CHO',
-                'case_decrease': 'Delay all Ex. 30 g CHO',
-                'insulin_rec': None,
-                'food_rec': None,
+                # 'case_increase': 'Delay all Ex. 20 g CHO',
+                # 'case_decrease': 'Delay all Ex. 30 g CHO',
+                # 'insulin_rec': None,
+                # 'food_rec': None,
+                # 'color': 'dark_yellow'
+                'case_decrease': {
+                    'text_AE': 'Delay exercise at least until reaching 90mg/dl and ➡️, ↗️ or ⬆️!',
+                    'text_other': 'Delay exercise at least until reaching 90mg/dl and ➡️, ↗️ or ⬆️!',
+                    'insulin_rec_AE': None,
+                    'insulin_rec_other': 'Insulin Correction needed!',
+                    'food_rec': '30'
+                },
                 'color': 'dark_yellow'
             }
             self.response_dict[self.response_enum.RESPONSE_16] = {
-                'case_increase': 'Delay all Ex. 25 g CHO',
-                'case_decrease': 'Delay all Ex. 35 g CHO',
-                'insulin_rec': None,
-                'food_rec': None,
+                # 'case_increase': 'Delay all Ex. 25 g CHO',
+                # 'case_decrease': 'Delay all Ex. 35 g CHO',
+                # 'insulin_rec': None,
+                # 'food_rec': None,
+                # 'color': 'dark_yellow'
+                'case_decrease': {
+                    'text_AE': 'Delay exercise at least until reaching 90mg/dl and ➡️, ↗️ or ⬆️!',
+                    'text_other': 'Delay exercise at least until reaching 90mg/dl and ➡️, ↗️ or ⬆️!',
+                    'insulin_rec_AE': None,
+                    'insulin_rec_other': 'Insulin Correction needed!',
+                    'food_rec': '35'
+                },
                 'color': 'dark_yellow'
             }
             self.response_dict[self.response_enum.RESPONSE_17] = {
-                'case_increase': 'Individual amount CHO Ingestion. Delay all Ex',
-                'case_decrease': 'Individual amount CHO Ingestion. Delay all Ex',
-                'insulin_rec': None,
-                'food_rec': None,
+                # 'case_increase': 'Individual amount CHO Ingestion. Delay all Ex',
+                # 'case_decrease': 'Individual amount CHO Ingestion. Delay all Ex',
+                # 'insulin_rec': None,
+                # 'food_rec': None,
+                # 'color': 'dark_yellow'
+                'case_decrease': {
+
+                    'text_AE': 'Delay exercise at least until reaching 90mg/dl and ➡️, ↗️ or ⬆️!',
+                    'text_other': 'Delay exercise at least until reaching 90mg/dl and ➡️, ↗️ or ⬆️!',
+                    'insulin_rec_AE': None,
+                    'insulin_rec_other': 'Insulin Correction needed!',
+                    'food_rec': 'Individual amount Carbohydrate ingestion'
+                },
                 'color': 'dark_yellow'
             }
 
             #Red Area
             self.response_dict[self.response_enum.RESPONSE_18] = {
-                'case_increase': 'Individual amount CHO Ingestion. Delay all Ex',
-                'case_decrease': 'Individual amount CHO Ingestion. Delay all Ex',
-                'insulin_rec': None,
-                'food_rec': None,
+                # 'case_increase': 'Individual amount CHO Ingestion. Delay all Ex',
+                # 'case_decrease': 'Individual amount CHO Ingestion. Delay all Ex',
+                # 'insulin_rec': None,
+                # 'food_rec': None,
+                # 'color': 'red'
+                'case_decrease': {
+                    'text_AE': 'Delay exercise at least until reaching 90mg/dl and ➡️, ↗️ or ⬆️!',
+                    'text_other': 'Delay exercise at least until reaching 90mg/dl and ➡️, ↗️ or ⬆️!',
+                    'insulin_rec_AE': None,
+                    'insulin_rec_other': None,
+                    'food_rec': 'Individual amount Carbohydrate ingestion'
+                },
                 'color': 'red'
             }
 
